9177434bfcf0619ee302548ff5c6e85f86d02217
Fisac <isac_degerborg@hotmail.com>
2020-02-29 12:18:06 +0100

    Player can now take damage

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,18 +1,29 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour, IDamageable
 {
 	public int life;
+	public GameManager gameManager;
 
-	private void Respawn()
+	private void Awake()
 	{
-
+		if (gameManager == null)
+			gameManager = FindObjectOfType<GameManager>();
 	}
 
 	public void TakeDamage(int damage, Vector2 direction)
 	{
+		
+		HandleDamage(damage);
+	}
+
+	private void HandleDamage(int damage)
+	{
 		life -= damage;
+		if (life <= 0)
+			gameManager.GameOver();
 	}
 }
