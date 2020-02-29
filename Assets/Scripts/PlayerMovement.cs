3a7500e0ca2e3e0564007495d1888cd1e18bd76f
Fisac <isac_degerborg@hotmail.com>
2020-02-29 11:05:26 +0100

    Added player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	public float speed;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+		HandleMovement();
+    }
+
+	private void HandleMovement()
+	{
+		Vector3 direction = Vector3.zero;
+
+		if (Input.GetAxis("Horizontal") > 0)
+		{
+			direction += Vector3.right;
+		}
+		if (Input.GetAxis("Horizontal") < 0)
+		{
+			direction += Vector3.left;
+		}
+
+		if (Input.GetAxis("Vertical") > 0)
+		{
+			direction += Vector3.up;
+		}
+		if (Input.GetAxis("Vertical") < 0)
+		{
+			direction += Vector3.down;
+		}
+
+		direction.Normalize();
+		transform.position += direction * Time.deltaTime * speed;
+
+	}
+}
