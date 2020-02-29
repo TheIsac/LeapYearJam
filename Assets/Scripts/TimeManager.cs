3fdbce5404f84d84983cc1c8d7bda713e503a46a
Winteractive_Marcus <winteractiveSE@gmail.com>
2020-02-29 11:03:02 +0100

    Added the TimeManager and a GameManager

--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float targetTimeScale;
+    private const float fixedDeltaMagnitude = 0.02f;
+
+    public static void Initialize()
     {
-        
+        TimeManager me = new GameObject("@TimeManager").AddComponent<TimeManager>();
+        me.SetUp();
     }
 
-    // Update is called once per frame
+    private void SetUp()
+    {
+        targetTimeScale = 1f;
+    }
+
     void Update()
     {
-        
+        Time.timeScale = Mathf.Lerp(Time.timeScale, targetTimeScale, Time.deltaTime);
+        Time.fixedDeltaTime = Time.timeScale * fixedDeltaMagnitude;
+    }
+
+    public void SetTimeScaleTarget(float value)
+    {
+        targetTimeScale = value;
+    }
+
+    public void ResetTimeScale()
+    {
+        targetTimeScale = 1;
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = fixedDeltaMagnitude;
     }
 }
