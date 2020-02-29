a1f63ac3991b1d7317823e52e03d05be6045cfb6
Fisac <isac_degerborg@hotmail.com>
2020-02-29 11:49:53 +0100

    Added player prefab and player health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+	public int life;
+
+	private void Respawn()
+	{
+
+	}
+
+	public void TakeDamage(int damage, Vector2 direction)
+	{
+		life -= damage;
+	}
+}
