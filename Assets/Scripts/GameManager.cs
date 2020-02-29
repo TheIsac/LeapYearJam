db2e76087be50cd87d8fe6915c9ccfa3b99c5559
Fisac <isac_degerborg@hotmail.com>
2020-02-29 15:28:11 +0100

    Added score system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,21 +3,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 	public GameObject loseText;
+	public TextMeshProUGUI levelText;
+	public LevelMaker levelMaker;
 
 	private bool gameIsOver;
+	private float score;
 
     void Start()
     {
         TimeManager.Initialize();
+		StartCoroutine(ScoreTick());
     }
 
 	private void Update()
 	{
 		HandleInput();
+		levelText.text = "Score: " + score.ToString();
 	}
 
 	private void HandleInput()
@@ -37,5 +43,14 @@ public class GameManager : MonoBehaviour
     {
 		loseText.SetActive(true);
 		gameIsOver = true;
+	}
+
+	private IEnumerator ScoreTick()
+	{
+		do
+		{
+			score += levelMaker.levelNumber * Time.deltaTime;
+			yield return new WaitForSecondsRealtime(1);
+		} while (true);
 	}
 }
