314f0981282e93f304b96fae60dbb30541921fa6
Winteractive_Marcus <winteractiveSE@gmail.com>
2020-02-29 16:14:37 +0100

    Added zoom on slowdown

---
+++ Assets/TimeScaleZoomer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScaleZoomer : MonoBehaviour
+{
+    Camera cam;
+    float startZoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+        startZoom = cam.orthographicSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float targetZoom = startZoom + (0.8f * Time.timeScale);
+
+
+
+        cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, targetZoom, Time.unscaledDeltaTime * 1.5f);
+    }
+}
